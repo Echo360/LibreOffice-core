af930e5a63cc77bf331590b807f19bf53559e12a
Jan Holesovsky <kendy@collabora.com>
2014-05-28 18:59:15 +0200

    libgltf: Add patch to move FPS to a different place.
    Change-Id: I9500e431b7eb763ef2a8b70a887a1b24cb1c0579

--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -18,6 +18,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libgltf,1))
 $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
 	external/libgltf/patches/include_path_glew.patch \
 	external/libgltf/patches/include_path_freetype.patch \
+	external/libgltf/patches/smaller_fps.diff \
 	external/libgltf/patches/rgba_textures.patch \
 	external/libgltf/patches/deallocation_fix.patch \
 ))
