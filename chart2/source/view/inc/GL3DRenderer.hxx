0fa05cf3f830c0a0c9b16733dfb993e3d64e165b
weigao <weigao@multicorewareinc.com>
2014-05-28 13:35:40 +0800

    add batch render function

--- chart2/source/view/inc/GL3DRenderer.hxx
+++ chart2/source/view/inc/GL3DRenderer.hxx
@@ -232,6 +232,7 @@ private:
     void GetBatchMiddleInfo(Extrude3DInfo &extrude3D);
     void InitBatch3DUniformBlock();
     void UpdateBatch3DUniformBlock();
+    void RenderBatchBars();
 private:
 
     struct ShaderResources
