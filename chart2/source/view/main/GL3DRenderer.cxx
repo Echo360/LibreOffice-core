7fce7e9916b0471310319b360bca99b96cec7eba
weigao <weigao@multicorewareinc.com>
2014-06-17 23:29:09 -0700

    fix the using 3.0 version bug
    Change-Id: Ie197d0156a2d8b54fe6b08a905d82a63d31513f2

--- chart2/source/view/main/GL3DRenderer.cxx
+++ chart2/source/view/main/GL3DRenderer.cxx
@@ -172,7 +172,6 @@ void OpenGL3DRenderer::CheckGLSLVersion()
     if (iVersion > 3)
     {
         maResources.m_b330Support = true;
-        maResources.m_b330Support = false;
         return;
     }
     p++;
